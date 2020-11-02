98ad36d7c5decdcde20335559302edb1eb0fd07c
M <proyectos@cubic-lab.com>
2020-11-02 16:56:41 +0100

    [F] buscador y loop de resultados en plantilla

---
+++ busqueda-profesionales.php
@@ -0,0 +1,61 @@
+<?php
+/*
+* Template Name: Busqueda Profesionales
+*/
+
+// cosas de WP
+
+boldthemes_set_override();
+
+get_header();
+
+$action_slug = $wp_query->query_vars['name'];
+
+?>
+
+<section class="boldSection topSpaced bottomSemiSpaced gutter inherit">
+
+    <form role="search" method="get" class="search-form" action="">
+        <label>
+            <span class="screen-reader-text"><?php echo _x('Buscar:', 'label') ?></span>
+            <input type="search" class="search-field" placeholder="<?php echo esc_attr_x('Introduzca su búsqueda...', 'placeholder') ?>"
+                   value="<?php echo get_search_query() ?>" name="s"
+                   title="<?php echo esc_attr_x('Buscar:', 'label') ?>"/>
+        </label>
+
+        <input type="submit" class="search-submit" value="<?php echo esc_attr_x('Buscar', 'submit button') ?>"/>
+    </form>
+
+</section>
+
+<?php
+
+if ( isset( $_GET['search-submit'] ) ) {
+
+    // global de WP para la base de datos
+    global $wpdb;
+    // Haciendo query para todos los usuarios
+    $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}posts WHERE post_type = 'erforms_submission' and post_title like '%2016%'", OBJECT );
+    // escapando la query de busqueda
+    $keyword = sanitize_text_field($_POST['s']);
+    // aquí guardaremos los resultados que coincidan
+    $coincidences = [];
+
+    foreach ($results as $result) {
+        // decodificamos el post_content
+        $result_array = json_decode($result->post_content);
+        // si hay alguna coincidencia
+        if( array_search_partial($result_array, $keyword) ) {
+            // guardamos el resultado sin serializar
+            $coincidences[] = $result_array;
+        }
+    }
+
+    foreach($coincidences as $coincidence) {
+        // AQUI SE MUESTRA EL RESULTADO
+        var_dump($coincidence);
+    }
+
+}
+
+get_footer();
--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
-function my_theme_enqueue_styles() {
 
-	$parent_style = 'parent-style'; // Estos son los estilos del tema padre recogidos por el tema hijo.
+function my_theme_enqueue_styles()
+{
 
-	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css',	array ( $parent_style ), wp_get_theme()->get('Version'));
+    $parent_style = 'parent-style'; // Estos son los estilos del tema padre recogidos por el tema hijo.
+
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array($parent_style), wp_get_theme()->get('Version'));
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-?>
+
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+function array_search_partial($arr, $keyword) {
+    foreach($arr as $index => $string) {
+        if (strpos($string, $keyword) !== FALSE)
+            return $index;
+    }
+
+    return FALSE;
+}
